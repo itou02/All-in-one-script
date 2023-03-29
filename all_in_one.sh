b1ee56ccc75ea9278115d726ddc9fd302d577374
itou02 <zoe2636@kimo.com>
2023-03-29 15:17:31 +0800

    all-in-one-script 必裝軟體

---
+++ all_in_one.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Install update"
+sudo apt-get install update
+
+echo "Install upgrade"
+sudo apt-get install upgrade
+
+echo "Install net-tools"
+sudo apt-get install net-tools
+
+echo "Install git"
+sudo apt-get install git
+
+echo "Install nano"
+sudo apt-get install nano
+
+echo "Install python"
+sudo apt-get install python3
+
+echo "Install vim"
+sudo apt-get install vim
+
+echo "Install apache2"
+sudo apt-get install apache2
